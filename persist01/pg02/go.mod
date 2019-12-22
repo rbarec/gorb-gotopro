ecf1d56bafe2e5d3f4c473c0da2ebf2c459fab6c
Ronald.Barrera <ronald.barrera@gizlocorp.com>
2019-12-22 12:53:16 -0500

    FUNCIONA -  package database/sql  persistence PG02
    getAllPersonas  con paso por Referencia
    Slides
    -pendientes: fecha data

---
+++ persist01/pg02/go.mod
@@ -0,0 +1,5 @@
+module github.com/rbarec/gorb-gotopro/persist01/pg01
+
+go 1.13
+
+require github.com/lib/pq v1.2.0
---
+++ persist01/pg02/pg02.go
@@ -0,0 +1,112 @@
+package pg01
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+// go mod init github.com/rbarec/gorb-gotopro/persist01/pg01
+// go test
+
+type PersonaModel struct {
+	//	Id 		  bson.ObjectId `bson:"_id" json:"id" `
+	Id       uint16 `pg:"id" json:"id" `
+	Cdn      string `pg:"cdn"    json:"cdn"`
+	IdenTyp  string `pg:"identyp"    json:"identTyp"`
+	Iden     string `pg:"ident"    json:"Ident"`
+	Names    string `pg:"nmes"   json:"Names"`
+	SurNames string `pg:"snames" json:"SurNames"`
+	//	Fnac      time.Time `pg:"Fnac" json:"Fnac"`
+
+}
+
+// GLOBAL_VAR
+var db *sql.DB
+
+func InitDB(dataSourceName string) {
+	log.Println("InitDB (" + dataSourceName + "")
+	var err error
+	db, err = sql.Open("postgres", dataSourceName)
+	if err != nil {
+		log.Panic(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func Info() string {
+	log.Println("INFO")
+	return "gotoPRO -> persist01 -> pg01"
+}
+
+func Dao_getAll(iden string) []*PersonaModel {
+	fmt.Println("INVOKE::: Dao_getAll") //> rbfmt
+	lPersonsPTR := []*PersonaModel{}
+	fmt.Println(lPersonsPTR) //> rbfmt
+	lPersonsPTR, _ = AllPersonaModel()
+
+	//Iterar en un Slice
+	// 	for _, per := range *lPersonsPTR {  .\pg02.go:52:22: invalid indirect of lPersonsPTR (type []*PersonaModel)
+	for _, per := range lPersonsPTR {
+		fmt.Printf("Nombre ::: %s  %s\n", per.Names, per.SurNames)
+		fmt.Println("")
+	}
+
+	return lPersonsPTR
+}
+
+func AllPersonaModel() ([]*PersonaModel, error) {
+	fmt.Println("INVOKE::: AllPersonaModel") //> rbfmt
+	log.Println(db.Stats())
+	rows, err := db.Query("SELECT id, cdn, nmes, snames  FROM plt_pnj_tb") ///>@query
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+	log.Println("2.- MAKE Array")
+	bks := make([]*PersonaModel, 0)
+	for rows.Next() {
+		row := new(PersonaModel)
+		err := rows.Scan(&row.Id, &row.Cdn, &row.Names, &row.SurNames)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		log.Println(row)
+		bks = append(bks, row)
+	}
+	if err = rows.Err(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return bks, nil
+}
+
+func AllPersonaModel2() {
+	var strQuery = "SELECT id, cdn, nmes, snames  FROM plt_pnj_tb"
+	log.Println("QUERY ", strQuery)
+	rows, err := db.Query(strQuery)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	log.Println("2.- MAKE Array")
+	bks := make([]*PersonaModel, 0)
+	for rows.Next() {
+		row := new(PersonaModel)
+		err := rows.Scan(&row.Id, &row.Cdn, &row.Names, &row.SurNames)
+		if err != nil {
+			return
+		}
+		bks = append(bks, row)
+	}
+	if err = rows.Err(); err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
